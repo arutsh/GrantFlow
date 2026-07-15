ffbe1414e2510f5208bec736d8c63f778f36df6c
Norair Arutshyan <n.arutshyan@gmail.com>
2026-07-15 15:44:36 +0100

    feat: add @vercel/analytics integration to enhance performance tracking

--- frontend-typescript/src/main.tsx
+++ frontend-typescript/src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <App />
       <SpeedInsights />
+      <Analytics />
     </QueryClientProvider>
   </React.StrictMode>
 );
