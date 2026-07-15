70ed06fb45bda37f9df9e7337c51abfd44921cc5
Norair Arutshyan <n.arutshyan@gmail.com>
2026-07-15 14:47:36 +0100

    feat: Add Vercel Speed Insights integration and brand color constants

--- frontend-typescript/src/main.tsx
+++ frontend-typescript/src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App";
 // import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <SpeedInsights />
     </QueryClientProvider>
   </React.StrictMode>
 );
